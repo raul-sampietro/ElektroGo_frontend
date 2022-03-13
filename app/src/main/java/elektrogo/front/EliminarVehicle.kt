4341455b37db16f209ec0f2947dff7f17fdea500
unknown <simon.helmuth.oliva@est.fib.upc.edu>
2022-03-13 18:43:33 +0100

    Task#42_Afegeix boto eliminar vehicle

---
+++ app/src/main/java/elektrogo/front/EliminarVehicle.kt
@@ -0,0 +1,11 @@
+package elektrogo.front
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class EliminarVehicle : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_eliminar_vehicle)
+    }
+}
