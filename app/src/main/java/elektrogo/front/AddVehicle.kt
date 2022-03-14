eb0ed0d7fd36baed12b4ff5c9aa7e52e1dddc806
joel.cardona <joel.cardona@estudiantat.upc.edu>
2022-03-15 00:04:36 +0100

    Task#80_Afegeix controladors d'errors al introduir la matricula

--- app/src/main/java/elektrogo/front/AddVehicle.kt
+++ app/src/main/java/elektrogo/front/AddVehicle.kt
@@ -2,6 +2,7 @@ package elektrogo.front
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
 import android.text.TextUtils
 import android.widget.Button
 import android.widget.EditText
@@ -11,31 +12,55 @@ class AddVehicle : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_vehicle)
 
-        val LicensePlate: EditText = findViewById(R.id.LicensePlate)
-        val VehicleModel: EditText = findViewById(R.id.VehicleModel)
-        val SaveVehicleButton: Button = findViewById(R.id.SaveVehicleButton)
+        val licensePlate: EditText = findViewById(R.id.LicensePlate)
+        val vehicleModel: EditText = findViewById(R.id.VehicleModel)
+        val saveVehicleButton: Button = findViewById(R.id.SaveVehicleButton)
 
-        SaveVehicleButton.setOnClickListener {
-            if (TextUtils.isEmpty(LicensePlate.getText())) {
-                LicensePlate.setError("Aquest camp és obligatori")
+        saveVehicleButton.setOnClickListener {
+            var isValid = true
+            val licensePlateString = licensePlate.getText()
+
+            if (TextUtils.isEmpty(licensePlateString)) { //Check that licensePlate value is not null
+                licensePlate.setError(resources.getString(R.string.ObligatoryField))
+                isValid = false
+            } else {
+                isValid = checkLicensePlate(licensePlateString)
+                if(!isValid) licensePlate.setError(resources.getString(R.string.invalidLicensePlate))
             }
-            if (TextUtils.isEmpty(VehicleModel.getText())) {
-                VehicleModel.setError("Aquest camp és obligatori")
+
+            if (isValid && licensePlateString.length < 7){    //Check licensePlate length is not less than seven
+                licensePlate.setError(resources.getString(R.string.notEnoughCharactersLicensePlate))
+                isValid = false
             }
+
+            if (TextUtils.isEmpty(vehicleModel.getText())) {
+                vehicleModel.setError(resources.getString(R.string.ObligatoryField))
+                isValid = false
+            }
+
+            //if(isValid) //crida a la base de dades (per fer)
         }
     }
 
-    /*
-    override fun onResume(){
-        super.onResume()
-
-        SaveVehicleButton.setOnClickListener {
-            if (TextUtils.isEmpty(LicensePlate.getText())) {
-                LicensePlate.setError("Aquest camp es obligatori")
-
+    private fun checkLicensePlate(licensePlateToCheck: Editable): Boolean {
+        var isValid = true
+        for(i in licensePlateToCheck.indices){   //Check licensePlate has valid input
+            if (licensePlateToCheck.length == 7) { //Then it is a car
+                if (i < 4 && licensePlateToCheck[i] !in '0'..'9') {
+                    isValid = false
+                } else if (i >= 4 && (licensePlateToCheck[i] !in 'A'..'Z' && licensePlateToCheck[i] !in 'a'..'z')) {
+                    isValid = false
+                }
+            }
+            else { //Then it is a moped
+                if (i in 1..4 && licensePlateToCheck[i] !in '0'..'9') {
+                    isValid = false
+                } else if ((i >= 5 || i == 0) && (licensePlateToCheck[i] !in 'A'..'Z' && licensePlateToCheck[i] !in 'a'..'z')) {
+                    isValid = false
+                }
             }
         }
+        return isValid
     }
 
-     */
 }
