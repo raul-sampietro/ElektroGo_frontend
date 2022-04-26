9fa3f7aefb35ef4c6a5a5aa3f7e54e730ee43f79
raul-sampietro <raul.sampietro@estudiantat.upc.edu>
2022-04-26 22:57:47 +0200

    Task#115_Login a Fragment i sessio persistent

---
+++ app/src/main/java/elektrogo/front/ui/login/LoginFragment.kt
@@ -0,0 +1,82 @@
+package elektrogo.front.ui.login
+
+import android.content.ContentValues
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.SignInButton
+import com.google.android.gms.common.api.ApiException
+import com.google.android.gms.tasks.Task
+import elektrogo.front.R
+
+
+class LoginFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val view =  inflater.inflate(R.layout.fragment_login, container, false)
+
+        // Configure sign-in                         to request the user's ID, email address, and basic
+        // profile. ID and basic profile are included in DEFAULT_SIGN_IN.
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestEmail()
+            .build()
+
+        // Build a GoogleSignInClient with the options specified by gso.
+        val mGoogleSignInClient = GoogleSignIn.getClient(requireActivity(), gso);
+
+        // Check for existing Google Sign In account, if the user is already signed in
+        // the GoogleSignInAccount will be non-null.
+        val account = GoogleSignIn.getLastSignedInAccount(requireActivity())
+
+        // Set the dimensions of the sign-in button.
+        val signInButton: SignInButton = view.findViewById(R.id.sign_in_button)
+        signInButton.setSize(SignInButton.SIZE_STANDARD)
+
+        signInButton.setOnClickListener {
+            val signInIntent = mGoogleSignInClient.signInIntent
+            startActivityForResult(signInIntent, RC_SIGN_IN)
+        }
+
+        return view
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        // Result returned from launching the Intent from GoogleSignInClient.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            // The Task returned from this call is always completed, no need to attach
+            // a listener.
+            val task: Task<GoogleSignInAccount> = GoogleSignIn.getSignedInAccountFromIntent(data)
+            handleSignInResult(task)
+        }
+    }
+
+    private fun handleSignInResult(completedTask: Task<GoogleSignInAccount>) {
+        try {
+            val account = completedTask.getResult(ApiException::class.java)
+            // Signed in successfully, show authenticated UI.
+            val intent = Intent(activity, PostLoginActivity::class.java)
+            startActivity(intent)
+        } catch (e: ApiException) {
+            // The ApiException status code indicates the detailed failure reason.
+            // Please refer to the GoogleSignInStatusCodes class reference for more information.
+            Log.w(ContentValues.TAG, "signInResult:failed code=" + e.statusCode)
+        }
+    }
+}
--- app/src/main/java/elektrogo/front/ui/login/PostLoginActivity.kt
+++ app/src/main/java/elektrogo/front/ui/login/PostLoginActivity.kt
@@ -1,12 +1,22 @@
 package elektrogo.front.ui.login
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.auth.api.signin.GoogleSignIn
 import elektrogo.front.R
+
 
 class PostLoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_post_login)
+
+        var text: TextView = findViewById(R.id.textBox)
+        val acct = GoogleSignIn.getLastSignedInAccount(this)
+        if (acct != null) {
+            text.text = "Hello, ${acct.displayName}!\n${acct.givenName}\n${acct.familyName}\n${acct.email}\n${acct.id}\n${acct.photoUrl}"
+        }
+        else text.text = "nullaccount"
     }
 }
