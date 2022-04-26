83f6d5af9a46368943b335cf2df103d84c453c4e
1251829 <adria.abad@estudiantat.upc.edu>
2022-04-26 13:55:15 +0200

    Task#168_Continua amb la implementacio. Avança vista conversation

--- app/src/main/java/elektrogo/front/ui/chatConversation/ChatConversation.kt
+++ app/src/main/java/elektrogo/front/ui/chatConversation/ChatConversation.kt
@@ -1,13 +1,25 @@
 package elektrogo.front.ui.chatConversation
 
 import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.ActionBar
 import androidx.appcompat.app.AppCompatActivity
 import elektrogo.front.R
 
 
 class ChatConversation : AppCompatActivity() {
+
+    private lateinit var toolbar: ActionBar
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val nameUserB : TextView? = findViewById(R.id.NameofUserB)
+        nameUserB?.text ?:  "UserB"
         setContentView(R.layout.activity_chat_conversation)
+    }
+
+    override fun onStart() {
+        super.onStart()
+
     }
 }
