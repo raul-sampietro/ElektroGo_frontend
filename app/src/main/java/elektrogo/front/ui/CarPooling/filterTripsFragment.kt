b824c2076900d5e201dea7f5b9a6174f3666ff9c
MarinaAlapontVidal <marina.alapont@estudiantat.upc.edu>
2022-05-01 13:45:46 +0200

    Canvi en el doxygen

--- app/src/main/java/elektrogo/front/ui/CarPooling/filterTripsFragment.kt
+++ app/src/main/java/elektrogo/front/ui/CarPooling/filterTripsFragment.kt
@@ -1,5 +1,5 @@
 /**
- * @file filtrarTrajectesFragment.kt
+ * @file filterTripsFragment.kt
  * @author Marina Alapont
  * @date 12/04/2022
  * @brief Implementacio d'un fragment per tal de cercar trajectes.
@@ -39,7 +39,7 @@ import java.time.format.DateTimeFormatter
 
 
 /**
- * @brief La clase filtrarTrajectesFragment representa la GUI de la pantalla on l'usuari insereix les dades per cercar trajectes i on veu el llistat resultant.
+ * @brief La clase filterTripsFragment representa la GUI de la pantalla on l'usuari insereix les dades per cercar trajectes i on veu el llistat resultant.
  */
 class filterTripsFragment : Fragment() {
 
