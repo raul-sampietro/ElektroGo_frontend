2d6a929c87c810c2af3a9e26acae63239f538c0a
MarinaAlapontVidal <marina.alapont@estudiantat.upc.edu>
2022-05-01 11:52:15 +0200

    Avança amb la GUI

--- app/src/main/java/elektrogo/front/ui/CarPooling/tripDetails.kt
+++ app/src/main/java/elektrogo/front/ui/CarPooling/tripDetails.kt
@@ -1,8 +1,11 @@
 package elektrogo.front.ui.CarPooling
 
 import android.os.Bundle
+import android.widget.TextClock
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import elektrogo.front.R
+import org.w3c.dom.Text
 
 
 class tripDetails : AppCompatActivity() {
@@ -12,19 +15,37 @@ class tripDetails : AppCompatActivity() {
         val username = intent.getStringExtra("username")
         val startDate = intent.getStringExtra("startDate")
         val startTime = intent.getStringExtra("startTime")
-        val offeredSeats = intent.getStringExtra("offeredSeats")
-        val occupiedSeats = intent.getStringExtra("occupiedSeats")
+        val offeredSeats = intent.getIntExtra("offeredSeats", 1)
+        val occupiedSeats = intent.getIntExtra("occupiedSeats", 1)
         val restrictions = intent.getStringExtra("restrictions")
         val details = intent.getStringExtra("details")
-        val latitudeOrigin = intent.getStringExtra("latitudeOrigin")
-        val longitudeOrigin = intent.getStringExtra("longitudeOrigin")
         val originString = intent.getStringExtra("originString")
-        val latitudeDestination = intent.getStringExtra("latitudeDestination")
-        val longitudeDestination = intent.getStringExtra("longitudeDestination")
-        val destinationString = intent.getStringExtra("destinationString")
+        val destinationString= intent.getStringExtra("destinationString")
         val vehicleNumberPlate = intent.getStringExtra("vehicleNumberPlate")
 
-      //  val imageid = intent.getIntExtra("imageid", R.drawable.a)
+        val usernameText :TextView  = this.findViewById(R.id.usernameDetails)
+        val startDateText : TextView = this.findViewById(R.id.dateDetails)
+        val startTimeText : TextView = this.findViewById(R.id.timeDetails)
+        val seatsText : TextView = this.findViewById(R.id.occupiedseats)
+        val restrictionText : TextView = this.findViewById(R.id.restrictionsInfo)
+        val detailsText : TextView = this.findViewById(R.id.detailsInfo)
+        val origin: TextView = this.findViewById(R.id.originDetails)
+        val destination: TextView = this.findViewById(R.id.destDetails)
+
+        usernameText.text = username
+        startDateText.text=startDate
+        startTimeText.text=startTime
+        var occupied : String = (offeredSeats - occupiedSeats).toString()
+        occupied += "/"
+        occupied += offeredSeats.toString()
+        seatsText.text = occupied
+        restrictionText.text = restrictions
+        detailsText.text=details
+        origin.text = originString
+        destination.text=destinationString
+
+
+        //  val imageid = intent.getIntExtra("imageid", R.drawable.a)
 
     }
 }
