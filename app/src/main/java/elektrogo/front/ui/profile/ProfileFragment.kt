ecf897c95406bfc1f6f40c32f3c2019109511c20
Marc Castells <marc.casellsd@gmail.com>
2022-05-02 12:31:08 +0200

    Botó redirecionat i possar string al diccionari

--- app/src/main/java/elektrogo/front/ui/profile/ProfileFragment.kt
+++ app/src/main/java/elektrogo/front/ui/profile/ProfileFragment.kt
@@ -6,11 +6,15 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.activity.result.ActivityResultLauncher
 import androidx.fragment.app.Fragment
 import elektrogo.front.R
 import elektrogo.front.ui.vehicleAdd.AddVehicle
 import elektrogo.front.databinding.ProfileFragmentBinding
 import elektrogo.front.ui.vehicleList.VehicleListActivity
+import elektrogo.front.ui.vehicleList.VehicleListFragment
+import elektrogo.front.ui.vehicleList.VehicleListViewModel
 
 class ProfileFragment : Fragment() {
 
@@ -21,13 +25,22 @@ class ProfileFragment : Fragment() {
     private lateinit var viewModel: ProfileViewModel
     private var _binding: ProfileFragmentBinding? = null
     private val binding get() = _binding!!
+    private lateinit var resultLauncher: ActivityResultLauncher<Intent>
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.profile_fragment, container, false)
+        val view: View = inflater.inflate(R.layout.profile_fragment, container, false)
+
+        val buttonCars: Button = view.findViewById(R.id.AddVehicleButton)
+        buttonCars.setOnClickListener {
+            val intent = Intent(container?.context, VehicleListActivity::class.java)
+            startActivity(intent)
+        }
+
+        return view
     }
 
     override fun onDestroyView() {
