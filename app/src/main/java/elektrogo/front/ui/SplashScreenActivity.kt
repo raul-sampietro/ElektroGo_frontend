d8ce92742c3d835acf735515bd6efafaf2dca20a
raul-sampietro <raul.sampietro@estudiantat.upc.edu>
2022-05-22 11:22:09 +0200

    Task#301_Splash screen dinamica no funciona

--- app/src/main/java/elektrogo/front/ui/SplashScreenActivity.kt
+++ app/src/main/java/elektrogo/front/ui/SplashScreenActivity.kt
@@ -9,6 +9,7 @@ import elektrogo.front.ui.login.LoginActivity
 class SplashScreenActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
         startActivity(Intent(this, LoginActivity::class.java))
         finish()
     }
