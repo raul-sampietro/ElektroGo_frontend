c959038180ec89edaa4b9c9290b7291ba5e8bb09
raul-sampietro <raul.sampietro@estudiantat.upc.edu>
2022-05-24 23:52:01 +0200

    Task#287_Fix server IP

--- app/src/main/java/elektrogo/front/controller/FrontendController.kt
+++ app/src/main/java/elektrogo/front/controller/FrontendController.kt
@@ -25,11 +25,11 @@ import java.io.ByteArrayOutputStream
  */
 object FrontendController {
     // SERVER
-    //private const val URL_BASE_WB = "http://10.4.41.58:8080/"
-    //private const val URL_BASE = "http://10.4.41.58:8080"
+    private const val URL_BASE_WB = "http://10.4.41.58:8080/"
+    private const val URL_BASE = "http://10.4.41.58:8080"
     // HOME
-    private const val URL_BASE_WB = "http://192.168.1.82:8080/"
-    private const val URL_BASE = "http://192.168.1.82:8080"
+    // private const val URL_BASE_WB = "http://192.168.1.82:8080/"
+    // private const val URL_BASE = "http://192.168.1.82:8080"
     // MOBILE NETWORK
     //private const val URL_BASE_WB = "http://192.168.43.104:8080/"
     //private const val URL_BASE = "http://192.168.43.104:8080"
