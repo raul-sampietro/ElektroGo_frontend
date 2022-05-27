4973ebea06d4d5f03c6f716c7e529d50c91435b0
joel.cardona <joel.cardona@estudiantat.upc.edu>
2022-05-27 18:41:31 +0200

    Task#322_Afegir la logica per inserir les imatges del dni

--- app/src/main/java/elektrogo/front/ui/vehicleList/VehicleListActivity.kt
+++ app/src/main/java/elektrogo/front/ui/vehicleList/VehicleListActivity.kt
@@ -32,8 +32,8 @@ class VehicleListActivity : AppCompatActivity() {
 
         var httpResponse : Pair<Int, Driver?> = viewModel.getDriver(SessionController.getUsername(this))
         if (httpResponse.first == 432) loadFragment(formNewDriverFragment)
-        else if (httpResponse.first == 200 && httpResponse.second!!.status == "pending") loadFragment(verifyingDriverFragment)
-        else if (httpResponse.first == 200 && httpResponse.second!!.status == "verified") loadFragment(vehicleListFragment)
+        else if (httpResponse.first == 200 && httpResponse.second!!.status == "pendent") loadFragment(verifyingDriverFragment)
+        else if (httpResponse.first == 200 && httpResponse.second!!.status == "verificat") loadFragment(vehicleListFragment)
         else {
             Toast.makeText(this, resources.getString(R.string.VehicleCreatedSuccessfully), Toast.LENGTH_LONG).show()
         }
